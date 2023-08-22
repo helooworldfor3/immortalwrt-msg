4480e1e5734fc24140768db4d688a91eb790741e
helooworldfor3 <81641175+helooworldfor3@users.noreply.github.com>
2023-08-22 19:09:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,13 +7,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
-
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-
-# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/ImmortalWrt/E8820S/g' package/base-files/files/bin/config_generate
-
 # 修改开源驱动wifi名称
 sed -i 's/OpenWrt/ZTE-E8820S-%s/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
@@ -23,25 +16,12 @@ sed -i 's/OpenWrt_2G/ZTE-E8820S/g' package/lean/mt-drivers/mt_wifi/files/mt7603.
 # 修改闭源驱动5G wifi名称
 sed -i 's/OpenWrt_5G/ZTE-E8820S-5G/g' package/lean/mt-drivers/mt_wifi/files/mt7612.dat
 
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
-
-# 修改banne文件（banne文件在根目录）（不要修改此行代码,怕弄的diy-lede.sh文件全失效,不需要的话前面加#，或者全行代码删除了）
-#rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./banner openwrt/package/base-files/files/etc/ && cd openwrt
-
-# 更改时区
-sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
-# Add jd
-# cd immortalwrt/package/ctcgfw/  
-# rm -rf ../ctcgfw/luci-app-jd-dailybonus
-# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git  
-
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
 
-# Add dnsfilter
-# git clone --depth=1 https://github.com/garypang13/luci-app-dnsfilter
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Add OpenAppFilter
-#git clone --depth=1 https://github.com/destan19/OpenAppFilter
+# Modify hostname
+#sed -i 's/OpenWrt/360T7/g' package/base-files/files/bin/config_generate
